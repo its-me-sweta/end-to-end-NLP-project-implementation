301ff5a2a57d4268f8b66117ce6217dd7713dea3
its-me-sweta <swetajha1096@gmail.com>
2024-07-28 18:38:30 +0530

    added packages

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="its-me-sweta",
+    author_email="swetajha1096@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
